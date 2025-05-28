a580c79e89ed00f083e85fbc8405f3578d9f9fc9
Armando DBG <bollain@gmail.com>
2025-05-28 15:18:32 -0700

    Update 11.5

--- src/hooks/useStylePanel.js
+++ src/hooks/useStylePanel.js
@@ -183,13 +183,7 @@ const useStylePanel = ({ selectedAnnotations, currentTool }) => {
       updateFromTool(currentTool);
       setShowLineStyleOptions(getDataWithKey(mapToolNameToKey(currentToolName)).hasLineEndings);
     }
-  }, [selectedAnnotation, currentTool, selectedAnnotations, i18n.language]);
-
-  const getColorFromHex = (hex) => {
-    const colorRGB = hexToRGBA(hex);
-    const color = new Annotations.Color(colorRGB.r, colorRGB.g, colorRGB.b, colorRGB.a);
-    return color;
-  };
+  }, [selectedAnnotation, currentTool]);
 
   const onStyleChange = (property, value, doneStyleChange = true) => {
     setStyle((previousStyle) => {
