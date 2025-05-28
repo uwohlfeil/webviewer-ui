c1d46599b0d61f25b1cc916ef3bfca8a3245be45
Armando DBG <bollain@gmail.com>
2025-05-28 15:18:32 -0700

    Update 11.5

--- src/redux/officeEditorModularComponents.js
+++ src/redux/officeEditorModularComponents.js
@@ -48,7 +48,6 @@ const defaultOfficeEditorModularHeaders = {
     style: {},
     items: [
       'officeEditorHomeGroupedItems',
-      'officeEditorLayoutGroupedItems',
       'officeEditorInsertGroupedItems',
       // 'officeEditorReviewGroupedItems'
     ]
@@ -345,7 +344,6 @@ const defaultOfficeEditorModularComponents = {
     dataElement: 'office-editor-default-ribbon-group',
     items: [
       'toolbarGroup-oe-Home',
-      'toolbarGroup-oe-Layout',
       'toolbarGroup-oe-Insert',
       // 'toolbarGroup-oe-Review',
     ],
